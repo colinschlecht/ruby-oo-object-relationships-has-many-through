bf1dea2a9fa2953dd73f24ecd795dc5958e663a3
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-19 01:10:12 -0800

    commit tree e29067ba78b86e3a5318d92cc3fc7e58e5928049

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
 class Customer
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all <<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #no need to enter customer, just reference self
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+#the customer can look back on every meal they've had with the meals method.
+#now we can iterate through every meal, and return the ones the customer
+#of this instance ate.
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  #here we can get every waiter they've ever been served by.
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
   
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,49 @@
 class Waiter
+    
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @@all << self
+        @name = name
+        @yrs_experience = yrs_experience
+    end
+
+    def self.all
+        @@all
+    end
+
+   #we dont need to take the waiter as an arguement becasue that will
+   #be passed on as a refrence through self
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    #to get all the meals they have served, create a meals method
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    #To find the best tipper, we can write another method, 
+    #best_tipper, use the array we get from #meals, then 
+    #return the customer of the meal with the highest tip:
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+      
+        best_tipped_meal.customer
+      end
+
+    #   customers have access to waiters and vice versa. 
+    #   Neither the customer nor waiter needed additional attributes.
+    #   THey only need methods that ask the right questions --
+    #   in this case to the meal class, the join between the customer and waiter.
 
 end
